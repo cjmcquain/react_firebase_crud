8ef9dbdf45c7e3f22dc8656c63819d06c9d3daf5
Charles McQuain <charlesmcquain@gmail.com>
2020-08-26 15:44:40 -0700

    Added firebase, config, Contacts and ContactForm components

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAUUFuCjspbu5PyUChMvbu_2x2DzCLSj7g",
+    authDomain: "react-firebase-crud-e3133.firebaseapp.com",
+    databaseURL: "https://react-firebase-crud-e3133.firebaseio.com",
+    projectId: "react-firebase-crud-e3133",
+    storageBucket: "react-firebase-crud-e3133.appspot.com",
+    messagingSenderId: "500056499762",
+    appId: "1:500056499762:web:5981c3b8338cd2d8277e2b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
