7478496d5b67552844b57d0c797d1d20dc77446a
Charles McQuain <charlesmcquain@gmail.com>
2020-08-26 16:47:30 -0700

    Added fields to contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,63 @@
+import React, {useState, useEffect} from 'react';
+
+
 const ContactForm = () => {
-    return (  );
+    const initialFieldValues = {
+        fullName: '',
+        mobile: '',
+        email: '',
+        address: ''
+    }
+
+    let [values, setValues] = useState(initialFieldValues);
+
+    const handleInputChange = e => {
+        let {name, value} = e.target
+        setValues({
+            ...values,
+            [name]: value
+        })
+    }
+
+    return ( 
+        <form autoComplete="off">
+            <div className="form-group input-group">
+                <div className="input-grou-prepend">
+                    <div className="input-group-text">
+                        <i className="fas fa-user"></i>
+                    </div>
+                </div>
+                <input className="form-control" placeholder="Full Name" 
+                    name="fullName" value={values.fullName} onChange={handleInputChange} />
+            </div>
+            <div className="form-row">
+                <div className="form-group input-group">
+                    <div className="input-grou-prepend">
+                        <div className="input-group-text">
+                            <i className="fas fa-mobile-alt"></i>
+                        </div>
+                    </div>
+                    <input className="form-control" placeholder="Mobile" 
+                        name="mobile" value={values.mobile} onChange={handleInputChange} />
+                </div>
+                <div className="form-group input-group">
+                    <div className="input-grou-prepend">
+                        <div className="input-group-text">
+                            <i className="fas fa-envelope"></i>
+                        </div>
+                    </div>
+                    <input className="form-control" placeholder="Email" 
+                        name="email" value={values.email} onChange={handleInputChange} />
+                </div>
+                <div className="form-group">
+                    <textarea placeholder="Address" name="address" id="" cols="30" rows="10" className="form-control" onChange={handleInputChange}></textarea>
+                </div>
+                <div className="form-group">
+                    <input type="submit" value="Save" className="btn btn-primary btn-block" />
+                </div>
+            </div>
+        </form>
+     );
 }
  
 export default ContactForm;
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,5 +1,25 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+
 const Contacts = () => {
-    return (  );
+    return ( 
+        <>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4 text-center">Contact Register</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-5">
+                    <ContactForm />
+                </div>
+                <div className="col-md-7">
+                    <div>list of contacts</div>
+                </div>
+            </div>
+        </>
+
+     );
 }
  
 export default Contacts;
